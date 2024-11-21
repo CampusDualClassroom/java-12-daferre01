08ed6e6912c5b4b1f8e647d6ee338daeb0de3ee9
David Fernandez Reboredo <davidfr2001@hotmail.com>
2024-11-21 12:28:02 +0100

    Ejercicio hecho

--- src/main/java/com/campusdual/classroom/Exercise12.java
+++ src/main/java/com/campusdual/classroom/Exercise12.java
@@ -2,6 +2,7 @@ package com.campusdual.classroom;
 
 public class Exercise12 {
     public static void main(String[] args) {
+        //comprobacions
         Car myCar = new Car("Toyota", "Corolla", "Gasolina");
         System.out.println("Detalles del coche:");
         myCar.showDetails();
@@ -29,8 +30,8 @@ public class Exercise12 {
         System.out.println("Marcha actual: " + myCar.gear);
         myCar.showDetails();
         System.out.println("Intentando poner marcha atrás mientras el coche está en movimiento...");
-        myCar.accelerate();  // Acelera el coche
-        myCar.setReverse(true);  // No debería permitirlo
+        myCar.accelerate();
+        myCar.setReverse(true);
         System.out.println("Marcha actual: " + myCar.gear);
         myCar.showDetails();
         System.out.println("Deteniendo el coche...");
